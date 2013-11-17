0ed7bb09d4c56685cbc2e6920e61290e168d7c1d
Iain MacDonald <IJMacD@gmail.com>
2013-11-18 00:30:55 +0800

    Preparing for CSS Lesson
    change comment parsing code
    update styles

--- js/pa.js
+++ js/pa.js
@@ -1,22 +1,29 @@
 $(function(){
-	var regex = /\blang(?:uage)?(?:\s+|-)(\w+)(?: (preview))?\b/i;
+	var langRegex = /^[ \t]*lang(?:uage)?(?::[ \t]*|-)(\w+)[ \t]*$/im,
+		previewRegex = /^[ \t]*preview(?:|[ \t]*(\w*))[ \t]*$/im;
 	$('code').each(function(i,item){
 		var $item = $(item),
 			prevSibling = $item.parent()[0].previousSibling,
 			match,
 			lang,
-			updater;
+			updater,
+			comment;
 
 		if(prevSibling.nodeType == 3){
 			prevSibling = prevSibling.previousSibling;
 		}
 
 		if(prevSibling.nodeType == 8){
-			match = prevSibling.textContent.match(regex);
-			lang = match[1];
-			$item.addClass("language-"+lang);
+			comment = prevSibling.textContent;
 
-			if(match[2]){
+			match = comment.match(langRegex);
+			if(match) {
+				lang = match[1];
+				$item.addClass("language-"+lang);
+			}
+
+			match = comment.match(previewRegex);
+			if(match && match[1] != "no"){
 
 				updater = function(){
 					item.preview.attr("src", "data:text/html;charset=utf-8," + encodeURIComponent( $item.text() ) );
