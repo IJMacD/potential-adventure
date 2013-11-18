5ab661d85c930b9cb9611386862402cfeab8da35
Iain MacDonald <IJMacD@gmail.com>
2013-11-18 23:38:49 +0800

    Actually support css previews

--- js/pa.js
+++ js/pa.js
@@ -1,13 +1,23 @@
 $(function(){
-	var langRegex = /^[ \t]*lang(?:uage)?(?::[ \t]*|-)(\w+)[ \t]*$/im,
-		previewRegex = /^[ \t]*preview(?:|[ \t]*(\w*))[ \t]*$/im;
-	$('code').each(function(i,item){
+	var langRegex = /^[ \t]*lang(?:uage)?(?::[ \t]*|-)(\S+)[ \t]*$/im,
+		previewRegex = /^[ \t]*preview(?::[ \t]*(\S*))?[ \t]*$/im,
+		cssParentRegex = /^[ \t]*css\((\S*)\)[ \t]*$/im,
+		idRegex = /^[ \t]*id:[ \t]*(\S*)[ \t]*$/im,
+		lang = /\blang(?:uage)?-(?!\*)(\w+)\b/i,
+		negative = ['no','false'],
+		codeBlocks = {},
+		typeCounts = {};
+
+	$('pre code').each(function(i,item){
 		var $item = $(item),
 			prevSibling = $item.parent()[0].previousSibling,
 			match,
-			lang,
+			value,
 			updater,
-			comment;
+			getSource,
+			comment,
+			lang,
+			id;
 
 		if(prevSibling.nodeType == 3){
 			prevSibling = prevSibling.previousSibling;
@@ -23,11 +33,37 @@ $(function(){
 			}
 
 			match = comment.match(previewRegex);
-			if(match && match[1] != "no"){
+			value = match && match[1];
+			if(match && negative.indexOf(value) == -1){
 
 				updater = function(){
-					item.preview.attr("src", "data:text/html;charset=utf-8," + encodeURIComponent( $item.text() ) );
+					item.preview.attr("src", "data:text/html;charset=utf-8," + encodeURIComponent( getSource() ) );
 				};
+
+				match = value && value.match(cssParentRegex);
+				value = match && match[1];
+				if(match){
+					if(value && codeBlocks[value]){
+
+						// Apply this code block ass CSS to the specified code block id
+						getSource = function(){
+							var source = codeBlocks[value].text(),
+								style = "<style>" + $item.text() + "</style>";
+							return source + style;
+						};
+
+						// Watch parent block for changes as well
+						codeBlocks[value]
+							.on("keyup", updater)
+							.attr("contenteditable", true);
+					}
+					else return;
+				}
+				else {
+					getSource = function() {
+						return $item.text();
+					}
+				}
 
 				item.preview = $('<iframe>');
 				updater();
@@ -44,8 +80,48 @@ $(function(){
 					})
 					.attr("contenteditable", true);
 			}
+
+			match = comment.match(idRegex);
+			if(match){
+				id = match[1];
+			}
 		}
+
+		if(!id){
+			id = lang || getLanguage(item);
+
+			if(!typeCounts[id]){
+				typeCounts[id] = 1;
+			}
+			else {
+				typeCounts[id] += 1;
+			}
+
+			id = id + pad(typeCounts[id],2);
+		}
+
+		codeBlocks[id] = $item;
+
+		$item.attr("id", id);
 	});
 
 	Prism.highlightAll();
+
+	function pad(num, size) {
+		var s = num+"";
+		while (s.length < size) s = "0" + s;
+		return s;
+	}
+
+	function getLanguage(element){
+		var parent = element;
+
+		while (parent && !lang.test(parent.className)) {
+			parent = parent.parentNode;
+		}
+
+		if (parent) {
+			return (parent.className.match(lang) || [,''])[1];
+		}
+	}
 });
